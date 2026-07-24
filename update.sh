b9d38902b00a008f9ccd825a5e86ec6ae478c8fe
Andrea Peverelli <ndrpeverelli@gmail.com>
2026-07-24 02:59:21 +0000

    Release v1.0.2

--- update.sh
+++ update.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-git pull
+echo "Fetching releases..."
+git fetch --tags origin
+
+LATEST_TAG=$(git tag --sort=-v:refname | head -n1)
+
+if [[ -z "$LATEST_TAG" ]]; then
+    echo "No release tags found."
+    exit 1
+fi
+
+echo "Latest release: $LATEST_TAG"
+
+git checkout "$LATEST_TAG"
+
+echo "Installing..."
+
 install -m 755 brand-icon-tools /usr/local/bin/brand-icon-tools
 
