cb69e2ddf04e4909e11f40f28d46ba1490055e38
Andrea Peverelli <ndrpeverelli@gmail.com>
2026-07-31 18:15:34 +0200

    sync

--- extra/palette/baseline-srgb-palette/index.ts
+++ extra/palette/baseline-srgb-palette/index.ts
@@ -1,11 +1,5 @@
-import {argbFromHex, themeFromSourceColor } from "@material/material-color-utilities";
+import {argbFromHex, CorePalette} from "@material/material-color-utilities";
 
-const theme = themeFromSourceColor(argbFromHex("#f82506"), [
-	{
-		name: "phx-srgb-palette",
-		value: argbFromHex("#ff0000"),
-		blend: true,
-	},
-]);
+process.stdout.write(JSON.stringify(CorePalette.of(argbFromHex(process.argv[2])), null, null));
 
-console.log(JSON.stringify(theme, null, null));
+process.exit(0);
--- extra/palette/baseline-srgb-palette/index2.ts
+++
@@ -1,5 +0,0 @@
-import {argbFromHex, CorePalette} from "@material/material-color-utilities";
-
-process.stdout.write(JSON.stringify(CorePalette.of(argbFromHex(process.argv[2])), null, null));
-
-process.exit(0);
